93da23579cebf9f080a157575bc8f0b1d869ce84
Salvador Guzman <guzmansalv@gmail.com>
2018-06-26 00:56:12 -0700

    add h1 hello

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,9 @@
 #include <cheerp/clientlib.h>
 
 void webMain() noexcept {
-  client::console.log("Test");
+  client::console.log("Hello, world!");
+  client::HTMLElement* body = client::document.get_body();
+  client::HTMLElement* newElement = client::document.createElement("h1");
+  newElement->set_textContent("Hello World");
+  body->appendChild(newElement);
 }
